8d7292d5543e9f2d4f59be167459fade2d36a9ae
julyanling <m15967179810@163.com>
2017-11-25 22:48:04 +0800

    第一次上传

---
+++ assets/js/head/loader.js
@@ -0,0 +1,110 @@
+document.getElementById("header").innerHTML="<nav class=\"navbar navbar-default top-navbar\" role=\"navigation\">\
+                <a class=\"navbar-brand\" style=\" width: 290px;\"href=\"index.html\"><strong><i class=\"icon fa fa-plane\"></i> 读书会在线学习系统</strong></a>\
+                <div id=\"sideNav\" href=\"#\"><i class=\"fa fa-bars icon\"></i></div><form class=\"navbar-form navbar-right\"  style=\"margin-top:12px;\">\
+        <div class=\"form-group\">\
+          <input type=\"text\" class=\"form-control\" placeholder=\"Search\">\
+        </div>\
+        <button type=\"submit\" class=\"btn btn-default\">搜索课程</button>\
+      </form>\
+         <ul class=\"nav navbar-top-links navbar-right\">\
+                <li class=\"dropdown\">\
+                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\" aria-expanded=\"false\">\
+                        <i class=\"glyphicon glyphicon-play-circle\"></i> <i class=\"fa fa-caret-down\"></i>\
+                    </a>\
+                    <ul class=\"dropdown-menu dropdown-tasks\">\
+                        <li>\
+                            <a href=\"#\">\
+                                <div>\
+                                    <p>\
+                                        <strong>两学一做重要讲话</strong>\
+                                        <span class=\"pull-right text-muted\">28% Complete</span>\
+                                    </p>\
+                                    <div class=\"progress progress-striped active\">\
+                                        <div class=\"progress-bar progress-bar-info\" role=\"progressbar\" aria-valuenow=\"28\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 28%\">\
+                                            <span class=\"sr-only\">28% Complete</span>\
+                                        </div>\
+                                    </div>\
+                                </div>\
+                            </a>\
+                        </li>\
+                        <li class=\"divider\"></li>\
+                        <li>\
+                            <a href=\"#\">\
+                                <div>\
+                                    <p>\
+                                        <strong>十九大最新报告</strong>\
+                                        <span class=\"pull-right text-muted\">80% Complete</span>\
+                                    </p>\
+                                    <div class=\"progress progress-striped active\">\
+                                        <div class=\"progress-bar progress-bar-warning\" role=\"progressbar\" aria-valuenow=\"80\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 80%\">\
+                                            <span class=\"sr-only\">80% Complete</span>\
+                                        </div>\
+                                    </div>\
+                                </div>\
+                            </a>\
+                        </li>\
+                        <li class=\"divider\"></li>\
+                        <li>\
+                            <a href=\"#\">\
+                                <div>\
+                                    <p>\
+                                        <strong>十九大最新报告</strong>\
+                                        <span class=\"pull-right text-muted\">90% Complete</span>\
+                                    </p>\
+                                    <div class=\"progress progress-striped active\">\
+                                        <div class=\"progress-bar progress-bar-danger\" role=\"progressbar\" aria-valuenow=\"90\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 90%\">\
+                                            <span class=\"sr-only\">90% Complete</span>\
+                                        </div>\
+                                    </div>\
+                                </div>\
+                            </a>\
+                        </li>\
+                        <li class=\"divider\"></li>\
+                        <li>\
+                            <a class=\"text-center\" href=\"#\">\
+                                <strong>更多课程</strong>\
+                                <i class=\"fa fa-angle-right\"></i>\
+                            </a>\
+                        </li>\
+                    </ul>\
+                </li>\
+                <li class=\"dropdown\">\
+                    <a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\" aria-expanded=\"false\">\
+                        <i class=\"glyphicon glyphicon-user\"></i> <i class=\"fa fa-caret-down\"></i>\
+                    </a>\
+                    <ul class=\"dropdown-menu dropdown-user\">\
+                        <li><a href=\"#\"><i class=\"glyphicon glyphicon-book\"></i> 我的档案</a>\
+                        </li>\
+                        <li><a href=\"#\"><i class=\"glyphicon glyphicon-pencil\"></i> 读书心得</a>\
+                        </li>\
+                         <li><a href=\"#\"><i class=\"glyphicon glyphicon-list-alt\"></i> 我的成绩</a>\
+                        </li>\
+                        <li class=\"divider\"></li>\
+                        <li><a href=\"#\"><i class=\"fa fa-sign-out fa-fw\"></i> 退出登录</a>\
+                        </li>\
+                    </ul>\
+                </li>\
+            </ul>\
+        </nav>"
+
+document.getElementById("footer").innerHTML="<footer><p style=\"text-align:center;\">Copyright &copy; 2017.ZGJSU ITOBase.<a target=\"_blank\" href=\"http://www.zjgsu.com/\">联系我们</a></p>\
+                </footer>"
+
+document.getElementById("slid").innerHTML="<nav class=\"navbar-default navbar-side\" role=\"navigation\">\
+      <div class=\"sidebar-collapse\">\
+        <ul class=\"nav\" id=\"main-menu\">\
+          <li>\
+            <a class=\"active-menu\" href=\"personal_center.html\" id=\"center\"><i class=\"fa fa-dashboard\"></i> 我的学习</a>\
+          </li>\
+          <li>\
+          <a href=\"stu_file.html\" id=\"file\"><i class=\"fa fa-desktop\"></i> 我的档案</a>\
+          </li>\
+          <li>\
+            <a href=\"stu_word.html\" id=\"word\"><i class=\"fa fa-qrcode\"></i>读书心得</a>\
+          </li>\
+          <li>\
+            <a href=\"stu_score.html\" id=\"score\"><i class=\"fa fa-table\"></i> 我的成绩</a>\
+          </li>\
+        </ul>\
+      </div>\
+    </nav>"
---
+++ assets/js/head/loader_admin.js
@@ -0,0 +1,35 @@
+document.getElementById("header").innerHTML="<nav class=\"navbar navbar-default top-navbar\" role=\"navigation\">\
+                <a class=\"navbar-brand\" style=\" width: 290px;\"href=\"index.html\"><strong><i class=\"icon fa fa-plane\"></i> 读书会在线学习系统</strong></a>\
+                <div id=\"sideNav\" href=\"#\"><i class=\"fa fa-bars icon\"></i></div><form class=\"navbar-form navbar-right\"  style=\"margin-top:12px;\">\
+        <div class=\"form-group\">\
+          <input type=\"text\" class=\"form-control\" placeholder=\"Search\">\
+        </div>\
+        <button type=\"submit\" class=\"btn btn-default\">搜索</button>\
+        <button type=\"submit\" class=\"btn btn-default\">退出登录</button>\
+      </form>\
+        </nav>"
+
+document.getElementById("footer").innerHTML="<footer><p style=\"text-align:center;\">Copyright &copy; 2017.ZGJSU ITOBase.<a target=\"_blank\" href=\"http://www.zjgsu.com/\">联系我们</a></p>\
+                </footer>"
+
+document.getElementById("slid").innerHTML="<nav class=\"navbar-default navbar-side\" role=\"navigation\">\
+      <div class=\"sidebar-collapse\">\
+        <ul class=\"nav\" id=\"main-menu\">\
+          <li>\
+            <a class=\"active-menu\" href=\"personal_center.html\" id=\"video\"><i class=\"fa fa-dashboard\"></i> 视频管理</a>\
+          </li>\
+          <li>\
+          <a href=\"stu_file.html\" id=\"audio\"><i class=\"fa fa-desktop\"></i> 音频管理</a>\
+          </li>\
+          <li>\
+            <a href=\"stu_word.html\" id=\"teach\"><i class=\"fa fa-qrcode\"></i>教师管理</a>\
+          </li>\
+          <li>\
+            <a href=\"stu_score.html\" id=\"stu\"><i class=\"fa fa-table\"></i>学生管理</a>\
+          </li>\
+          <li>\
+            <a href=\"stu_score.html\" id=\"user\"><i class=\"fa fa-table\"></i>系统用户管理</a>\
+          </li>\
+        </ul>\
+      </div>\
+    </nav>"
